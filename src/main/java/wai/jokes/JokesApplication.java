d224ccfbdae70aae57343bdfb6859cb8d4bb42ac
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-09-24 15:10:29 +0200

    changed Java based configuration to xml based, so as to utilize a external dependency (ChuckNorrisQuotes) within the Spring application

--- src/main/java/wai/jokes/JokesApplication.java
+++ src/main/java/wai/jokes/JokesApplication.java
@@ -2,8 +2,12 @@ package wai.jokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+//Tell Spring to search the classpath for the file classpath:chuck-config.xml
+//and pull that in Spring Context
+@ImportResource("classpath:chuck-config.xml")
 public class JokesApplication {
 
     public static void main(String[] args) {
--- src/main/java/wai/jokes/config/ChuckConfiguration.java
+++ src/main/java/wai/jokes/config/ChuckConfiguration.java
@@ -1,19 +1,17 @@
 package wai.jokes.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 //Using a java configuration to include a bean of ChuckNorrisQuotes
 //This is useful when you are dealing with external jar from an open source project
 //and want to utilize that dependency with your application e.g. include it as a bean in the Spring Context
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
     //Adding the following to be a bean
     //Here making ChuckNorrisQuotes as a bean
-    @Bean
+    //@Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
